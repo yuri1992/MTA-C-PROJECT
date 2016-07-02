238e59a5377e2062bde0f6a1ff98920cc77e7ad6
RonnyTzur <tzuronny@gmail.com>
2016-07-02 15:08:45 +0300

    Update delete-apt.h

--- delete-apt.h
+++ delete-apt.h
@@ -2,6 +2,8 @@
 // Created by yurir on 6/23/16.
 //
 
+#include<stdio.h>
+#include<time.h>
 #include "lineinteperter.h"
 #include "apartment.h"
 
@@ -10,4 +12,11 @@
 
 void delete_apt(COMMAND cmd, ApartmentTable db);
 
+struct tm * datelessdays(int days);
+
+int sortDB(ApartmentTable db,int day, int mon, int year);
+void   deleteDBdays(ApartmentTable *db,int day,int mon,int year);
+Apartment* checkright(int left,int* right,ApartmentTable db,int day,int mon, int year);
+
+int sumdate(int day,int mon,int year);
 #endif //MTA_C_PROJECT_DELETE_APT_H
