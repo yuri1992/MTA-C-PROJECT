96592f9c1cc5a8b4ce5899fe19d32f1f71d02445
RonnyTzur <tzuronny@gmail.com>
2016-07-02 20:55:04 +0300

    Update buy-apt.c

--- buy-apt.c
+++ buy-apt.c
@@ -1,27 +1,25 @@
-//
-// Created by yurir on 6/23/16.
+//// Created by yurir on 6/23/16.
 //
 #include <stdio.h>
 #include "buy-apt.h"
 #include "apartment.h"
-#include "history.h"
-#include "find-apt.h"
+#include "lineinteperter.h"
 
-void buy_apt(COMMAND cmd, ApartmentTable db) 
+void buy_apt(COMMAND cmd, ApartmentTable db)
 {
-  int idnumber;
-  int i;
-  int size;
-  Apartment tmp;
-  sscanf(cmd,"%s, %*s %d",&idnumber);
-  size=db.size;
-  for (i=0;i<size;i++)
-  {
-    if(idnumber==db->(arr+i).id)
-    {
-      db->(arr+i)=db->(arr+size));
-      db.size=(size-1);
-      return;
-    }
-  }
+	int idnumber;
+	int i;
+	int size;
+	Apartment tmp;
+	sscanf(cmd.kwargs->arr->value, "%s, %*s %d", &idnumber);
+	size = db.size;
+	for (i = 0; i<size; i++)
+	{
+		if (idnumber == db.arr[i].id)
+		{
+			db.arr[i] = db.arr[size];
+			db.size = (size - 1);
+			return;
+		}
+	}
 }
